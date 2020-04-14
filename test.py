4d287d8c33579023849e3a5fffb16edbcb3f8bc8
Ryan Russell <ki4rex@gmail.com>
2020-04-13 20:55:24 -0400

    messing wit

--- test.py
+++ test.py
@@ -1,23 +1,33 @@
-from tkinter import *
+from Tkinter import *
 
-from tkinter import Menu
+def save():
+    text = e1.get() + "\n"+e2.get() + "\n"+e3.get() 
+    with open("test.txt", "w") as f:
+        f.writelines(text)
 
-window = Tk()
+def show():
+    with open("test.txt", "r") as f:
+        f.readlines()
+    e1.get(f.seek(0))
+    e2.get(f.seek(1))
+    e3.get(f.seek(2))
 
-window.title("Welcome to LikeGeeks app")
 
-menu = Menu(window)
+master = Tk(className = "ABM Inputs")
 
-new_item = Menu(menu)
+Label(master, text="RNG Seed").grid(row=0)
+Label(master, text="Manipulator Exists 1=yes, 0=no").grid(row=1)
+Label(master, text="Number of Investors").grid(row=2)
 
-new_item.add_command(label='New')
+e1 = Entry(master)
+e2 = Entry(master)
+e3 = Entry(master)
 
-new_item.add_separator()
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+e3.grid(row=2, column=1)
 
-new_item.add_command(label='Edit')
+Button(master, text='Save', command=save).grid(row=3, column=0, sticky=W, pady=4)
+Button(master, text='Show', command=show).grid(row=3, column=1, sticky=W, pady=4)
 
-menu.add_cascade(label='File', menu=new_item)
-
-window.config(menu=menu)
-
-window.mainloop()
+mainloop( )
